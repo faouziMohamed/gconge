a7a6a528c7098f22d7597c9d44bc2f81072e6b41
Faouzi Mohamed <57812398+faouziMohamed@users.noreply.github.com>
2022-09-24 10:28:59 +0100

    chore(migrtions): remove some column and rename others

---
+++ GConge.web.api/Migrations/20220923212221_RemovePhoneNumberColumn.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GConge.web.api.Migrations
+{
+    public partial class RemovePhoneNumberColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Email = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Firstname = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Lastname = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Password = table.Column<byte[]>(type: "longblob", nullable: false),
+                    Role = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    PasswordSalt = table.Column<byte[]>(type: "longblob", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    CreatedBy = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    LastModifiedDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    LastModifiedBy = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Service = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    CreatedBy = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    LastModifiedDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    LastModifiedBy = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Employees_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "LeaveRequests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    StartDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    DateRequested = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    DateUpdated = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    Status = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    RequestingEmployeeId = table.Column<int>(type: "int", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    CreatedBy = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    LastModifiedDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    LastModifiedBy = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LeaveRequests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LeaveRequests_Employees_RequestingEmployeeId",
+                        column: x => x.RequestingEmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "CreatedBy", "DateCreated", "Email", "Firstname", "LastModifiedBy", "LastModifiedDate", "Lastname", "Password", "PasswordSalt", "Role" },
+                values: new object[] { 1, "System", new DateTime(2022, 9, 23, 22, 22, 21, 184, DateTimeKind.Local).AddTicks(7294), "admin.email@email.com", "Admin", "System", new DateTime(2022, 9, 23, 22, 22, 21, 184, DateTimeKind.Local).AddTicks(7340), "Admin", new byte[] { 0, 246, 190, 224, 41, 33, 204, 251, 104, 169, 250, 56, 220, 161, 201, 117, 62, 103, 246, 64, 63, 62, 80, 50, 65, 147, 215, 215, 225, 222, 100, 99, 118, 6, 169, 97, 46, 163, 162, 50, 68, 207, 159, 165, 81, 141, 47, 25, 21, 97, 87, 3, 103, 32, 211, 168, 225, 28, 217, 90, 133, 191, 250, 21 }, new byte[] { 109, 129, 206, 88, 196, 0, 217, 106, 65, 180, 158, 41, 246, 137, 199, 181, 180, 41, 48, 4, 70, 176, 181, 229, 205, 29, 53, 77, 18, 122, 66, 168, 28, 24, 67, 70, 131, 177, 164, 75, 89, 193, 43, 33, 147, 161, 68, 37, 120, 155, 185, 205, 200, 25, 8, 57, 237, 220, 8, 243, 215, 106, 244, 162, 61, 48, 221, 56, 95, 69, 102, 158, 9, 26, 160, 87, 77, 28, 97, 237, 162, 169, 78, 119, 176, 144, 124, 182, 182, 240, 116, 167, 68, 247, 236, 14, 247, 12, 66, 95, 163, 170, 71, 29, 83, 191, 151, 142, 211, 151, 90, 255, 175, 59, 21, 48, 196, 155, 167, 23, 126, 106, 191, 85, 80, 172, 55, 181 }, "Admin" });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "CreatedBy", "DateCreated", "LastModifiedBy", "LastModifiedDate", "Service", "UserId" },
+                values: new object[] { 1, "System", new DateTime(2022, 9, 23, 22, 22, 21, 184, DateTimeKind.Local).AddTicks(7511), "System", new DateTime(2022, 9, 23, 22, 22, 21, 184, DateTimeKind.Local).AddTicks(7517), "IT", 1 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_UserId",
+                table: "Employees",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LeaveRequests_RequestingEmployeeId",
+                table: "LeaveRequests",
+                column: "RequestingEmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LeaveRequests");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
